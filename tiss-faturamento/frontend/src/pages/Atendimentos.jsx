3843aa75ec05729d6154e3707c9519a2e877a93f
MichaelRaimundo <michael.rodrigoraimundo@gmail.com>
2026-04-30 13:18:32 -0300

    Update Atendimentos.jsx

--- tiss-faturamento/frontend/src/pages/Atendimentos.jsx
+++ tiss-faturamento/frontend/src/pages/Atendimentos.jsx
@@ -14,6 +14,8 @@ import {
   UserGroupIcon,
   CalendarIcon,
   IdentificationIcon,
+  ChevronUpIcon,
+  ChevronDownIcon,
   ClockIcon
 } from '@heroicons/react/24/outline';
 import { toast } from 'sonner';
