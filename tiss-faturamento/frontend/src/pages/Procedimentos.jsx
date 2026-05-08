f87ffdf85e72538ad9e554a00b43b4a809bc55cf
MichaelRaimundo <michael.rodrigoraimundo@gmail.com>
2026-05-07 23:05:23 -0300

    Update Procedimentos.jsx

--- tiss-faturamento/frontend/src/pages/Procedimentos.jsx
+++ tiss-faturamento/frontend/src/pages/Procedimentos.jsx
@@ -665,7 +665,7 @@ export default function Procedimentos() {
                               )}
                             </div>
                           </td>
-                        </td>
+                        </tr>
                       )}
                     </>
                   );
