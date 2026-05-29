2757ac82a5c1ebd25ccb6e0526e0d0982b9df3a0
MichaelRaimundo <michael.rodrigoraimundo@gmail.com>
2026-05-28 23:21:42 -0300

    Consolidar arquivos com conflito de icones

--- tiss-faturamento/frontend/src/pages/Atendimentos.jsx
+++ tiss-faturamento/frontend/src/pages/Atendimentos.jsx
@@ -21,7 +21,7 @@ import {
   LockClosedIcon,
   LockOpenIcon,
   ArrowPathIcon,
-  ClipboardDocumentCheckIcon,
+  DocumentCheckIcon,
   ReceiptPercentIcon,
   CloudArrowUpIcon
 } from '@heroicons/react/24/outline';
@@ -2288,7 +2288,7 @@ export default function Atendimentos() {
                           className="p-1 rounded-lg text-purple-600 hover:bg-purple-50 dark:hover:bg-purple-900/20 transition-colors disabled:opacity-50" 
                           title="Imprimir Guia TISS"
                         >
-                          <ClipboardDocumentCheckIcon className="w-4 h-4" />
+                          <DocumentCheckIcon className="w-4 h-4" />
                         </button>
                         
                         {/* Botão de Imprimir Conta Faturada */}
